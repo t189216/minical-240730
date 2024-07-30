908539dd265bc72d93393348519d589040754484
T189216 <tus0926@naver.com>
2024-07-31 01:17:23 +0900

    fix: CREATE_DATE & MODIFY_DATE null

--- src/main/java/com/ll/minical_240730/domain/HomeController.java
+++ src/main/java/com/ll/minical_240730/domain/HomeController.java
@@ -1,9 +1,13 @@
 package com.ll.minical_240730.domain;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
+@Tag(name = "HomeController", description = "홈 컨트롤러")
 public class HomeController {
     @GetMapping("/")
     public String showMain() {
