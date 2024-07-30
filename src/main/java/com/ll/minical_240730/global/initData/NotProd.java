5d2772906a91a560dd342816c613cae4c8d41685
T189216 <tus0926@naver.com>
2024-07-31 01:06:24 +0900

    test: 회원 샘플 데이터

---
+++ src/main/java/com/ll/minical_240730/global/initData/NotProd.java
@@ -0,0 +1,32 @@
+package com.ll.minical_240730.global.initData;
+
+import com.ll.minical_240730.domain.member.entity.Member;
+import com.ll.minical_240730.domain.member.sevice.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@Profile("!prod")
+@Slf4j
+@RequiredArgsConstructor
+public class NotProd {
+    private final MemberService memberService;
+
+    @Bean
+    @Order(3)
+    public ApplicationRunner initNotProd() {
+        return args -> {
+            if (memberService.findByUsername("user1").isPresent()) return;
+
+            Member memberUser1 = memberService.join("user1", "1234").getData();
+            Member memberUser2 = memberService.join("user2", "1234").getData();
+            Member memberUser3 = memberService.join("user3", "1234").getData();
+            Member memberUser4 = memberService.join("user4", "1234").getData();
+        };
+    }
+}
