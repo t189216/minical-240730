c166e1aa1e178c41a611c4303f9c965452465572
T189216 <tus0926@naver.com>
2024-07-30 17:10:59 +0900

    start

---
+++ src/main/java/com/ll/minical_240730/Minical240730Application.java
@@ -0,0 +1,13 @@
+package com.ll.minical_240730;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Minical240730Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Minical240730Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/minical_240730/Minical240730ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.minical_240730;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Minical240730ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
